74bf3020c7afa86617a22ecd526e889568f92bad
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-24 16:28:28 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  
+  def initialize(name, artist, genre)
+    @@count += 1 
+  end 
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres 
+    @@genres = []
+  end 
+  
+  def self.artists
+    @@artists = []
+  end
+    
+  def self.genre_count
+  end
+  
+  def self.artist_count
+  end 
+  
+end 
